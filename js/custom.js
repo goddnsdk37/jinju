4b0d20172818448f2f3a6c9656af720f9df1c7b3
tubi55 <tubi55@nate.com>
2021-07-08 11:52:30 +0900

    2depth 메뉴 포커스 이벤트 연동

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ btnCall.onclick = function(e){
     menuMo.classList.toggle("on"); 
 }
 
-
+//마우스 호버시 2depth활성화
 $("#gnb>li").on("mouseenter", function(){
     $(this).find(".sub").show();
 });
@@ -16,3 +16,17 @@ $("#gnb>li").on("mouseenter", function(){
 $("#gnb>li").on("mouseleave", function(){
     $(this).find(".sub").hide();
 });
+
+//포커스 이동시 2depth활성화
+$("#gnb>li").each(function(index){
+
+    //1depth li에서 첫번째 a요소에 foucsin이벤트 연결
+    $("#gnb>li").eq(index).find("a").first().on("focusin", function(){
+        $("#gnb>li").eq(index).find(".sub").show();
+    });
+
+    //1depth li에서 마지막 a요소에 focusout이벤트 연결
+    $("#gnb>li").eq(index).find("a").last().on("focusout", function(){
+        $("#gnb>li").eq(index).find(".sub").hide();
+    })
+})
